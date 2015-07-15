1fae3a248409bae3fb48eb9a9da49a06a04b8c30
fumio_shimamura <sun.shimamura@gmail.com>
2015-07-15 14:12:00 +0000

    Implementation of the delete

--- message-board/app/controllers/messages_controller.rb
+++ message-board/app/controllers/messages_controller.rb
@@ -1,24 +1,50 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
   	@messages = Message.all
   	@message = Message.new
   end
-	def create
-  	@message = Message.new(message_params)
-		if @message.save
-			redirect_to root_path, notice:'メッセージを保存しました'
-		else
-	  	# メッセージが保存できなかった時
-	  	@messages = Message.all
-	  	flash.now[:alert] = "メッセージの保存に失敗しました。"
-	  	render 'index'
-	 	end
-	end
-	  
+  
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+	  redirect_to root_path, notice:'メッセージを保存しました'
+	else
+	  # メッセージが保存できなかった時
+	  @messages = Message.all
+	  flash.now[:alert] = "メッセージの保存に失敗しました。"
+	  render 'index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  	if @message.update(message_params)
+  	  #成功したらトップページに
+  	  redirect_to root_path, notice: 'メッセージを編集しました'
+  	else
+  	  #失敗したら編集画面に
+  	  render 'edit'
+  	end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
 	private
 	def message_params
 		# params[:message]のパラメータで name , bodyのみを許可する。
         # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
 		params.require(:message).permit(:name, :body)
 	end
+
+  def set_message
+  	@message = Message.find(params[:id])
+  end
+  
 end
